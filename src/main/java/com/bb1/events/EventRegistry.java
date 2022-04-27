bb64e36f21afa75d4760c1ada1ab2a2350965fa7
BradBot_1 <45739045+BradBot1@users.noreply.github.com>
2022-04-27 22:41:37 +0100

    jitpack

--- src/main/java/com/bb1/events/EventRegistry.java
+++ src/main/java/com/bb1/events/EventRegistry.java
@@ -10,6 +10,27 @@ import org.jetbrains.annotations.NotNull;
 
 import com.bb1.registry.SimpleRegistry;
 
+/**
+ * 
+ * Copyright 2022 BradBot_1
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * A simple registry to contain all events
+ * 
+ * @author BradBot_1
+ */
 final class EventRegistry extends SimpleRegistry<String, Event<?>> {
 	
 	private final @NotNull Set<BiConsumer<String, Event<?>>> registerHandlers = Collections.newSetFromMap(new ConcurrentHashMap<BiConsumer<String, Event<?>>, Boolean>());
