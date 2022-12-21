9b88002d8e3d62ef12bd0a7b2bc3c76a1804d05d
BradBot_1 <45739045+BradBot1@users.noreply.github.com>
2022-12-21 23:23:22 +0000

    Redo event handler now that I am sane?

--- src/main/java/fun/bb1/events/handler/IEventHandler.java
+++ src/main/java/fun/bb1/events/handler/IEventHandler.java
@@ -2,11 +2,9 @@ package fun.bb1.events.handler;
 
 import org.jetbrains.annotations.NotNull;
 
-import fun.bb1.events.Event;
-
 @FunctionalInterface
-public interface IEventHandler<E, T> {
+public interface IEventHandler<T> {
 	
-	public void handleEvent(@NotNull final Event<T> E);
+	public void handleEvent(@NotNull final T event);
 	
 }
